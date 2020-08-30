093cbee14ec96b71ff35a63df356dd674d871cd6
Jose Augusto Megres de Oliveira <49933483+augustomegres@users.noreply.github.com>
2020-08-30 12:35:00 -0300

    firstcommit: first commit

---
+++ theme/theme.js
@@ -0,0 +1,41 @@
+const light = {
+  primary: {
+    main: '#5c22d8',
+    text: '#ffffff',
+    success: '#32a852',
+    alert: '#fcba03',
+    dangerText: '#eb4034',
+    components: '#ffffff',
+    background: '#f7f9fc',
+    chart: {
+      dashboard: {
+        entrada: '#5c22d8',
+        saida: '#5c22d8',
+        diferença: '#5c22d8',
+      },
+    },
+  },
+  secondary: {
+    main: '#ff5cf0',
+    text: '#ffffff',
+  },
+};
+
+const dark = {
+  primary: {
+    main: '#5c22d8',
+    text: '#ffffff',
+    dangerText: '#eb4034',
+  },
+  secondary: {
+    main: '#ff5cf0',
+    text: '#ffffff',
+    components: '#ffffff',
+    background: '#f7f9fc',
+  },
+};
+
+export default {
+  light,
+  dark,
+};
